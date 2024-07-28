df82c2f62cf3dac8f4ee41608604a6212aba2065
Piotrek <Piotr.karys96@gmail.com>
2024-07-28 23:22:11 +0200

    Updates

---
+++ assets/chunk-7OLJDQMT-Cy4Lq46R.js
@@ -0,0 +1,1 @@
+import{g as i,A as t,p as r,j as c,f as m,h as o}from"./index-DLHKS1xb.js";var g=i(function(a,s){const e=t("Heading",a),{className:f,...n}=r(a);return c.jsx(m.h2,{ref:s,className:o("chakra-heading",a.className),...n,__css:e})});g.displayName="Heading";export{g as H};
